19b0e71df85fcc4359b6da9c2aa72a75834171ff
Paulo Toscano <opaulotoscano2@hotmail.com>
2022-07-15 15:45:16 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import Header from "./componentes/header"
 import CardExterno from "./componentes/cardExterno"
+
+
 
 function App() {
     return(
       <>
+      <ToastContainer />
         <Header />
         <main>
             <section id="areaVendas">
--- frontend/src/componentes/cardExterno/index.tsx
+++ frontend/src/componentes/cardExterno/index.tsx
@@ -1,6 +1,9 @@
-import { useState } from "react"
+import axios from "axios"
+import { useEffect, useState } from "react"
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
+import { Sale } from "../../models/sale"
+import { BASE_URL } from "../../utils/request"
 import NotificationButton from '../notificatoinButton'
 import './styles.css'
 
@@ -8,9 +11,22 @@ function CardExterno() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365))
     const max = new Date()
+
     const [dataMinima, setDataMinima] = useState(min)
     const [dataMaxima, setDataMaxima] = useState(max)
 
+    const [sales, setSales] = useState<Sale[]>([])
+
+    useEffect(() => {
+
+        const dMin = dataMinima.toISOString().slice(0, 10)
+        const dMax = dataMaxima.toISOString().slice(0, 10)
+
+        axios.get(`${BASE_URL}/sales?minDate=${dMin}&maxDate=${dMax}`)
+            .then(response => {
+                setSales(response.data.content)
+            })
+    }, [dataMinima, dataMaxima])
 
     return(
         <div className="cardInterno">
@@ -48,71 +64,23 @@ function CardExterno() {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">11/07/2022</td>
-                            <td>Anakin</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="container-botao-vermelho">
-                                    <NotificationButton />
-                                </div>
-                            </td>                                        
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">11/07/2022</td>
-                            <td>Ana</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="container-botao-vermelho">
-                                <NotificationButton />
-                                </div>
-                            </td>                                        
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">11/07/2022</td>
-                            <td>Ana</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="container-botao-vermelho">
-                                <NotificationButton />
-                                </div>
-                            </td>                                        
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">11/07/2022</td>
-                            <td>Ana</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="container-botao-vermelho">
-                                <NotificationButton />
-                                </div>
-                            </td>                                        
-                        </tr>
-                        <tr>
-                            <td className="show992">#341</td>
-                            <td className="show576">11/07/2022</td>
-                            <td>Ana</td>
-                            <td className="show992">15</td>
-                            <td className="show992">11</td>
-                            <td>R$ 55.300,00</td>
-                            <td>
-                                <div className="container-botao-vermelho">
-                                <NotificationButton />
-                                </div>
-                            </td>                                        
-                        </tr>
+                        {sales.map(sale => {
+                            return(
+                                <tr key={sale.id}>
+                                    <td className="show992">{sale.id}</td>
+                                    <td className="show576">{new Date(sale.date).toLocaleDateString()}</td>
+                                    <td>{sale.sellerName}</td>
+                                    <td className="show992">{sale.visited}</td>
+                                    <td className="show992">{sale.deals}</td>
+                                    <td>{sale.amount.toFixed(2)}</td>
+                                    <td>
+                                        <div className="container-botao-vermelho">
+                                            <NotificationButton saleId={sale.id}/>
+                                        </div>
+                                    </td>                                        
+                                </tr> 
+                            )
+                        })}
                     </tbody>
                 </table>
             </div>
